c05c9a9ba94cbfdafb0ad9d43728037b54b69e4f
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-17 11:13:20 -0300

    Verifying user in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +18,40 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          // Verificando se o usuário já existe
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              // nome da tabela que queremos inserir
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get( //select do mysql
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+  
+        return true
+      } catch{
+        return false
+      }
+    },
+  },
 })
