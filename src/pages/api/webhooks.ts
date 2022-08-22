39ffdcabb8184c675ebd2a0ec85bf7baa9560159
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-21 23:53:50 -0300

    saving events data

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 // converte a readable string num objeto
 async function buffer(readable: Readable){
@@ -43,8 +44,26 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const { type } = event
   
-  if (!relevantEvents.has(type)){
-    console.log('Evento recebido', event)
+  if (relevantEvents.has(type)){
+    try {
+      switch (type) {
+        case 'checkout.session.completed':
+
+          const checkoutSession = event.data.object as Stripe.Checkout.Session
+        
+          await saveSubscription(
+            checkoutSession.subscription.toString(),
+            checkoutSession.customer.toString()
+          )
+
+          break;
+        default:
+          throw new Error('Unhandled event.')
+      }
+    } catch (err) {
+      // o ideal seria avisar através de um sentry ou bugsnag
+      return res.json ({ error: 'Webhook handler failed.' })
+    }
   }
 
   res.json({ received: true})
